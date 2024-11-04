72863146e99c0c20a8792b6d902f81d671378c14
James Pike <jamesandrewpike@gmail.com>
2024-11-04 14:27:49 -0500

    commit

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,17 +1,116 @@
-import { component$, Slot } from "@builder.io/qwik";
+import { component$, useStore, Slot } from "@builder.io/qwik";
+import { useContent } from "@builder.io/qwik-city";
+
+import ToggleTheme from "~/components/common/ToggleTheme";
+import ToggleMenu from "~/components/common/ToggleMenu";
 
 import Footer from "~/components/widgets/Footer";
-import Header from "~/components/widgets/Header";
+import { Logo } from "~/components/widgets/Logo";
+import IconChevronDown from "~/components/icons/IconChevronDown";
+
 
 export default component$(() => {
-  
-  
+  const store = useStore({
+    isScrolling: false,
+  });
+
+  const { menu } = useContent();
+
   return (
     <>
-      <Header />
+      {/* Header */}
+      <header
+        id="header"
+        class={`sticky top-0 z-40 bg-gradient-to-r from-blue-200 to-slate-300 flex-none mx-auto w-full border-b border-gray-50/0 transition-[opacity] ease-in-out ${
+          store.isScrolling
+            ? " md:bg-white/90 md:backdrop-blur-sm dark:md:bg-slate-900/90 bg-white dark:bg-slate-900"
+            : ""
+        }`}
+        window:onScroll$={() => {
+          if (!store.isScrolling && window.scrollY >= 10) {
+            store.isScrolling = true;
+          } else if (store.isScrolling && window.scrollY < 10) {
+            store.isScrolling = false;
+          }
+        }}
+      >
+        <div class="absolute inset-0"></div>
+        <div class="relative text-default py-2 px-3 md:px-6 mx-auto w-full md:flex md:justify-between max-w-7xl">
+          <div class="mr-auto rtl:mr-0 rtl:ml-auto flex justify-between">
+            <a class="flex items-center" href={"/"}>
+              <Logo />
+            </a>
+            <div class="flex items-center md:hidden">
+              <a
+                href="/contact"
+                class="btn btn-primary bg-primary-700 ml-2 py-2.5 px-4 md:px-6 font-semibold shadow-none text-sm w-auto"
+              >
+                Contact Us
+              </a>
+              <ToggleMenu />
+            </div>
+          </div>
+          <nav
+            class="items-center w-full md:w-auto hidden md:flex text-default overflow-y-auto overflow-x-hidden md:overflow-y-visible md:overflow-x-auto md:mx-5"
+            aria-label="Main navigation"
+          >
+            {menu && menu.items ? (
+              <ul class="flex flex-col md:flex-row md:self-center w-full md:w-auto text-xl md:text-[0.9375rem] tracking-[0.01rem] font-medium">
+                {menu.items.map(({ text, href, items }, key) => (
+                  <li key={key} class={items?.length ? "dropdown" : ""}>
+                    {items?.length ? (
+                      <>
+                        <button class="hover:text-link dark:hover:text-white px-4 py-3 flex items-center">
+                          {text}{" "}
+                          <IconChevronDown class="w-3.5 h-3.5 ml-0.5 rtl:ml-0 rtl:mr-0.5 hidden md:inline" />
+                        </button>
+                        <ul class="dropdown-menu md:backdrop-blur-md dark:md:bg-slate-800 rounded md:absolute pl-4 md:pl-0 md:hidden font-medium md:bg-white/90 md:min-w-[200px] drop-shadow-xl">
+                          {items.map(({ text: text2, href: href2 }, key2) => (
+                            <li key={key2}>
+                              <a
+                                class="first:rounded-t last:rounded-b md:hover:bg-gray-100 hover:text-link dark:hover:text-white dark:hover:bg-gray-700 py-2 px-5 block whitespace-no-wrap"
+                                href={href2}
+                              >
+                                {text2}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      </>
+                    ) : (
+                      <a class="hover:text-link dark:hover:text-white px-4 py-3 flex items-centers" href={href}>
+                        {text}
+                      </a>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+          </nav>
+          <div class="hidden md:self-center md:flex items-center md:mb-0 fixed w-full md:w-auto md:static justify-end left-0 rtl:left-auto rtl:right-0 bottom-0 p-3 md:p-0">
+            <div class="items-center flex justify-between w-full md:w-auto">
+              <div class="flex">
+                <ToggleTheme iconClass="w-6 h-6 md:w-5 md:h-5 md:inline-block" />
+              </div>
+              <span class="ml-4 rtl:ml-0 rtl:mr-4">
+                <a
+                  href="/contact"
+                  class="btn btn-primary ml-2 py-2.5 px-5.5 md:px-6 font-semibold shadow-none text-sm w-auto"
+                >
+                  Contact Us
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* Main Content */}
       <main>
         <Slot />
       </main>
+
+      {/* Footer */}
       <Footer />
     </>
   );

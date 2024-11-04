96a0753c2eacf8ec4760f1f6a62493a2b8e28577
James Pike <jamesandrewpike@gmail.com>
2024-11-04 13:41:47 -0500

    commit

--- src/components/widgets/Logo.tsx
+++ src/components/widgets/Logo.tsx
@@ -3,16 +3,14 @@ import { component$ } from "@builder.io/qwik";
 // @ts-ignore
 import logoSrc from "~/assets/images/logo.png?width=120&height=40&png";
 
-export default component$(() => (
+
+export const Logo = component$(() => (
   <span class="self-center ml-2 text-2xl md:text-xl font-bold text-gray-900 whitespace-nowrap dark:text-white flex items-center">
     <img
       src={logoSrc}
       class="inline-block mr-1"
       width={120}
       height={40}
-      alt="Qwind Logo"
-      
-    />
-    
+      alt="Qwind Logo"/>
   </span>
 ));
